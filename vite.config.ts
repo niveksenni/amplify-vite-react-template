439c446db675d4f7745946f5a4d16e2db687e2c0
KevinInnes <kevin@agnify.io>
2025-05-15 13:46:16 +0200

    delete todo item

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import PinyVite from '@pinegrow/piny-vite' // <-- import the plugin
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), PinyVite()]
 })
